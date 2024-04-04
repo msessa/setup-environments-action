ab5f7755f970407b6c02fb0741aa9a9dc449412d
Matteo Sessa <matteo.sessa@service.vic.gov.au>
2024-04-04 15:19:09 +1100

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,63 @@
+import { github, javascript } from 'projen';
+import { GitHubActionTypeScriptProject, RunsUsing } from 'projen-github-action-typescript';
+
+const project = new GitHubActionTypeScriptProject({
+  defaultReleaseBranch: 'main',
+  devDeps: [
+    'projen-github-action-typescript',
+    'esbuild',
+  ],
+  name: 'setup-environments-action',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  depsUpgradeOptions: {
+    workflow: false,
+  },
+  minMajorVersion: 1,
+  license: 'MIT',
+  copyrightOwner: 'Service Victoria',
+  actionMetadata: {
+    author: 'Service Victoria Platform Engineering',
+    name: 'Setup Environments',
+    description: 'Action to configures environments in a repository',
+    runs: {
+      using: RunsUsing.NODE_20,
+      main: 'dist/index.js',
+    },
+    inputs: {
+      token: {
+        description: 'Github token with the scope "repo"',
+        required: true,
+      },
+      repository: {
+        description: 'Repository name with owner. For example, sv-oss/repo',
+        required: false,
+        default: '${{ github.repository }}',
+      },
+      environments: {
+        description: 'Comma-separated list of Environments to configure',
+        required: true,
+      },
+      reviewers: {
+        description: 'Comma-separated list of required reviewers e.g. "org/team,user" max 6',
+        required: false,
+      },
+    },
+  },
+});
+
+project.release?.addJobs({
+  'floating-tags': {
+    permissions: {
+      contents: github.workflows.JobPermission.WRITE,
+    },
+    runsOn: ['ubuntu-latest'],
+    needs: ['release_github'],
+    steps: [
+      { uses: 'actions/checkout@v4' },
+      { uses: 'giantswarm/floating-tags-action@v1' },
+    ],
+  },
+});
+
+project.synth();
--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# setup-environments-action
-
-## Inputs
-- `token` - GitHub token with `repo` scope
-- `repository` - Repository name with owner. For example, sv-oss/repo
-- `environments` - Comma-separated list of Environments to configure
-- `reviewers` - Comma-separated list of required reviewers e.g. 'org/team,user' max 6
+# replace this
